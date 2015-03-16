38dffa5e6149ed79312f6d29a7c897552fba8cb3
marciomacielbastos <marciomacielbastos@hotmail.com>
2015-03-16 15:29:11 -0300

    adicionei o det

--- matriz.cpp
+++ matriz.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class Matrix{
@@ -7,9 +8,14 @@ class Matrix{
     private: float** matrix;
     public: Matrix():matrix(NULL),rows(0),cols(0){}
     public: Matrix(int rows,int cols):rows(rows),cols(cols){
-        matrix =new (nothrow) float*[rows];
-        for(int i=0;i<3;i++){
-            matrix[i]=new (nothrow) float[cols];
+        try{
+            matrix =new float*[rows];
+            for(int i=0;i<3;i++){
+                matrix[i]=new float[cols];
+            }
+        }
+        catch(exception e){
+            cerr<<e.what()<<endl;
         }
     }
     public: Matrix(float **matrix){
@@ -32,12 +38,69 @@ class Matrix{
             }
         }
     }
+    public: float det(){
+        try{
+            float det=1,p;
+            int I=0;
+            float a;
+            if(rows==cols){
+                float **mat=new float *[rows];
+                for(int i=0;i<3;i++){
+                    mat[i]=new float[cols];
+                }
+                for(int i=0;i<rows;i++){
+                    for(int j=0;j<cols;j++){
+                        mat[i][j]=matrix[i][j];;
+                    }
+                }
+                while(I<rows && det!=0){
+                    pivot(I,mat,&det);
+                    p=mat[I][I];
+                    det=det*p;
+                    if(p){
+                        for(int j=I;j<rows;j++){
+                            mat[I][j]=mat[I][j]/p;
+                        }
+                        for(int k=I+1;k<rows;k++){
+                            p=mat[k][I];
+                            for(int j=I;j<rows;j++){
+                                mat[k][j]=mat[k][j]-p*mat[I][j];
+                            }
+                        }
+
+                    }I++;
+                }
+            }
+            return det;
+        }
+        catch(exception e){
+            cerr<<e.what()<<endl;
+        }
+    }
+    private: void pivot(int I,float **mat,float *det){
+        try{
+            float c=mat[I][I];
+            int l=I;
+            for(int k=I+1;k<rows;k++){
+                if(abs(c)<abs(mat[k][I])){
+                    c=mat[k][I];
+                    l=k;
+                }
+            }
+            if(l!=I){
+                *det=-*det;
+                for(int j=0;j<cols;j++){
+                    swap(mat[I][j],mat[l][j]);
+                }
+            }
+        }
+        catch(exception e){
+            cerr<<e.what()<<endl;
+        }
+    }
     public: void setMatrix(float **matrix){
         this->matrix=matrix;
     }
-
-
-
 };
 
 int main ()
@@ -52,11 +115,13 @@ int main ()
         for(int j=0;j<3;j++){
             matrix[i][j]=i+j+k;
         }
-        k+=32.15;
+        k+=2;
     }
     Matrix matrixx=Matrix(matrix);
     Matrix matriz=Matrix(3,3);
     matriz.setMatrix(matrix);
+    matriz.setMatrix();
+    cout<<"det="<<matriz.det()<<endl;
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             cout<<matriz.getMatrix()[i][j]<<" ";
